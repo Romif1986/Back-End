139589f3dec62b2fde884e552a7e1281b084f7a0
Romif1986 <romi_f1986@hotmail.com>
2023-05-16 22:22:47 -0300

    arreglos

--- src/main/java/com/argProg/tp/security/controller/AuthController.java
+++ src/main/java/com/argProg/tp/security/controller/AuthController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://miportfoliorf.firebaseapp.com", "http://localhost:4200"})
+@CrossOrigin(origins = "https://miportfoliorf.firebaseapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
